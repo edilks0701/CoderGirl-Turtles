2891de09b7e6c6435ca8e4a7e2169c4f0926c8aa
Jonny McFadden <jmcfadden1528@gmail.com>
2019-02-27 10:38:25 -0600

    Completed Turtles

--- Turtles/Program.cs
+++ Turtles/Program.cs
@@ -7,8 +7,16 @@ namespace Turtles
         public static void Main()
         {
             // TODO: Prints "I like turtles." 1000 times.
+            string turtles = "I like turtles.";
+            int counter = 0;
+            while (counter < 1001)
+            {
+                Console.WriteLine(turtles);
+                counter++;
+            }
 
             Console.ReadLine();
         }
+              
     }
 }
